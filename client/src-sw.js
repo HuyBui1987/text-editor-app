17a5f30d45d8d626b825fb5f858ea7dd57236d63
HuyBui1987 <86652591+HuyBui1987@users.noreply.github.com>
2022-07-25 17:04:45 +1000

    client side folder

---
+++ client/src-sw.js
@@ -0,0 +1,42 @@
+import { warmStrategyCache } from 'workbox-recipes';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200]
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60
+    })
+  ]
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+const cacheName = 'asset-cache';
+const matchCallback = ({ request }) => ['style', 'script', 'worker'].includes(request.destination);
+registerRoute(
+  matchCallback,
+  new StaleWhileRevalidate({
+    cacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
